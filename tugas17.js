585e4481f58d513b813100b11a42bb3af8be7e78
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-22 23:43:56 +0700

    tugas 17 muhammad rizwan

---
+++ tugas17.js
@@ -0,0 +1,23 @@
+/* tugas 17 javascript */
+// Muhammad Rizwan
+
+function tugasTujuhBelas(){
+// 1). ubah kalimat ke lowerCase dan upperCase
+    var kalimat1 = "Saya beLajar bahaSa peMrograman deNgan khUsuk";
+    console.log("1).Ubah kalimat menjadi lowercase dan uppercase :",);
+
+    console.log(kalimat1.toLowerCase()); // lower case
+    console.log(kalimat1.toUpperCase()); // upper case
+
+    var kalimat2 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual.";
+    console.log(kalimat2.toLowerCase()) // lower case
+    console.log(kalimat2.toUpperCase()) // upper case
+
+// 2). tentukan kode ASCII
+    var ascii = ('Lorem Ipsum');
+    console.log("2).Tentukan Kode ASCII :",);
+
+    console.log(ascii.charCodeAt(0)+'-'+ascii.charCodeAt(1)+'-'+ascii.charCodeAt(2)+'-'+ascii.charCodeAt(3)+'-'+ascii.charCodeAt(4)+'-'+ascii.charCodeAt(5));
+}
+
+tugasTujuhBelas();
